0eadb4a9eb6e39cbf580f4f2c7abec6e1fe1f165
Anna Nardella <nardella.anna@gmail.com>
2023-03-06 14:30:49 +0100

    is_binary_search tree with memoization

--- DataStructures/is_bst_acn.py
+++ DataStructures/is_bst_acn.py
@@ -19,17 +19,101 @@ class Tree(object):
         self.keys = keys
         self.lchilds = lchilds
         self.rchilds = rchilds
+        self.minmax = [None]*len(self.keys)
 
     def __str__(self):
         s = "Tree: \n"
         s = s + "keys: " + str(self.keys) +"\n"
         s = s + "lchilds: " + str(self.lchilds) + "\n"
         s = s + "rchilds: " + str(self.rchilds) + "\n"
+        s = s + "minmax: " + str(self.minmax) + "\n"
         return s
 
 
     def get_root(self):
         return self.keys[0]
+
+
+    def get_minmax(self, i=0):
+        """
+        if the subtree at i meets the bst property, returns a tuple (min, max) of the
+        biggest and smallest values in that subtree, otherwise returs False
+
+        use the following memoization:
+        - if the subtree rooted at i meets the bst property, in the list self.minmax[i] 
+        we store a tuple (min,max) of the biggest and smallest key values to be found in the 
+        subtree rooted at node i, inclusive of node i itself
+        - if the subtree rooted at i does NOT meet bst property, we memoize False
+
+        NB for bst property we want:
+              min <= k and max >= k 
+        (we include equals because the min or max could be the value of k itself)
+
+
+        """
+        k = self.keys[i]        # the key value k of node i
+       # print("call minmax on node", k)
+
+        ileft = self.lchilds[i]
+        iright = self.rchilds[i]
+
+        ## CHECK MEMOIZATION
+        if self.minmax[i] is not None:
+         #   print("using memoized value")
+            return self.minmax[i]
+
+
+        ## BASE CASE, i is a leaf, so its key k is only value in the subtree rooted at i
+        if ileft == -1 and iright == -1:
+            self.minmax[i] = (k,k)
+         #   print("node", k, "is a leaf")
+            return self.minmax[i]            # memoize the value and return
+
+        ## BASE CASE, i's immediate left or right child is too big/small
+        if (ileft !=-1 and self.keys[ileft] > k) or (iright!=-1 and self.keys[iright] < k):
+                self.minmax[i]= False
+                return False            
+
+        ## RECURSIVE CALLS
+        lmin, lmax, rmin, rmax = k, k, k, k
+
+        if ileft != -1:                       #left child exists
+            left = self.get_minmax(ileft)
+            if left is False:                 #left subtree is invalid
+                self.minmax[i]= False
+                return False
+            else:                             
+                lmin, lmax = left             #left subtree valid 
+                if lmax > k:                  # but contains items bigger than k
+                    self.minmax[i]= False
+                    return False                    
+
+
+        if iright != -1:                        #right child exists
+            right = self.get_minmax(iright)
+            if right is False:                 #right subtree is invalid
+                self.minmax[i]= False
+                return False
+            else:                             
+                rmin, rmax = right             #right subtree valid 
+                if rmin < k:                  # but contains items smaller than k
+                    self.minmax[i]= False
+                    return False                    
+
+
+        smallest = min(k, lmin, rmin)
+        biggest = max(k, lmax, rmax)
+
+        self.minmax[i] = (smallest, biggest)  #memoize and return
+      #  print("memoizing", self.minmax[i], "for node", k)
+        return self.minmax[i]
+
+
+
+
+
+
+
 
     def is_binary_search_tree(self, i=0):
         """
@@ -37,6 +121,9 @@ class Tree(object):
         it satisfies the binary search tree property with recursion 
 
         returns True if bst, otherwise False
+
+        NB. THIS VERSION WILL NOT WORK if the node violating the binary property
+        is further down in the tree (ie not an immediate child)
         """
 
         k = self.keys[i] # the key value k of node i
@@ -109,21 +196,39 @@ def test():
     return tr
 
 def test_bst():
-    keys = [2, 1, 2]
-    lchilds = [1, -1, -1]
-    rchilds = [3, -1, -1] 
+    # SAMPLE1
+    # keys = [2, 1, 3]
+    # lchilds = [1, -1, -1]
+    # rchilds = [2, -1, -1] 
 
-    # keys = [1, 1, 2]
-    # lchilds = [2, -1, -1]
-    # rchilds = [3, -1, -1]
+    # SAMPLE2
+    # keys = [1, 2, 3]
+    # lchilds = [1, -1, -1]
+    # rchilds = [2, -1, -1]
+
+    # SAMPLE4
+    # keys = [1, 2, 3, 4, 5]
+    # lchilds = [-1,-1,-1,-1,-1]
+    # rchilds = [1,2,3,4,-1]
+
+    #SAMPLE5
+    # keys = [4, 2, 6, 1, 3, 5, 7]
+    # lchilds = [1, 3, 5, -1, -1, -1, -1]
+    # rchilds = [2, 4, 6, -1, -1, -1, -1]
+
+    # SAMPLE 6
+    keys = [4, 2, 1, 5]
+    lchilds = [1, 2, -1, -1]
+    rchilds = [-1, 3, -1, -1]
+
 
     tr = Tree(keys, lchilds, rchilds)
     print(tr)
 
-    if tr.is_binary_search_tree():
-        print("CORRECT")
-    else:
+    if tr.get_minmax(0) is False:
         print("INCORRECT")
+    else:
+        print("CORRECT")
 
 def main():
     """
@@ -140,6 +245,12 @@ def main():
     lchild = []
     rchild = []
 
+    ## EMPTY TREE OR TREE WITH ONLY A ROOT ALWAYS CORRECT
+    if n<=1:
+        print("CORRECT")
+        return
+
+    ## AT LEAST TWO NODES, CHECK BINARY PROPERTY
     for i in range(n):
         [k, l, r] = [int(s) for s in input().split()] 
         key.append(k)
@@ -152,12 +263,18 @@ def main():
 
     tr = Tree(key, lchild, rchild)
     # print(tr)
+    
+    if tr.get_minmax(0) is False:
+        print("INCORRECT")
+    else:
+        print("CORRECT")
 
-    tr.order("in")
-    print()
-    tr.order("pre")
-    print()
-    tr.order("post")
+
+    # tr.order("in")
+    # print()
+    # tr.order("pre")
+    # print()
+    # tr.order("post")
 
 # if __name__ == '__main__':
 
